f3aca75550a6fc25e2029be8d78ceafef2353ed3
Wessel Smit <wrsmit00@gmail.com>
2020-02-03 14:40:55 +0100

    Update getData.js

--- modules/getData.js
+++ modules/getData.js
@@ -1,5 +1,5 @@
 export default async function getData() {
     const rawData = await fetch("https://opentdb.com/api.php?amount=10&category=25&difficulty=medium");
     const jsonData = await rawData.json();
-    return jsonData;
+    return jsonData.results;
 }
