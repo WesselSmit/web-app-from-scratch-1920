5992bf5e3d488eb585fc81570aae92e13f947a04
Wessel Smit <wrsmit00@gmail.com>
2020-02-04 12:58:18 +0100

    manifest fixed + responsiveness fixed

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ let fetchedData,
         "type": "multiple"
     }
 
-getData(triviaSettings)
-    // .then(response => {
-    //     if (response.ok) {
-    //         console.log(response)
-    //     } else {
-    //         return Promise.reject(response)
-    //     }
-    // })
-    .then(jsonData => fetchedData = jsonData)
-    .then(() => console.log(fetchedData))
-    .catch(err => console.log(err))
+document.querySelector('section:first-of-type button').addEventListener('click', () => {
+    triviaSettings.limit = +document.getElementById('limit').value
+    triviaSettings.category = +document.getElementById('category').value
+    triviaSettings.difficulty = document.getElementById('difficulty').value
+    fetchData(triviaSettings)
+    document.getElementById('settings').classList.toggle('hidden')
+    document.getElementById('questions').classList.toggle('hidden')
+})
+
+
+function fetchData(triviaSettings) {
+    getData(triviaSettings)
+        .then(jsonData => fetchedData = jsonData)
+        .then(() => console.log(fetchedData))
+        .catch(err => console.log(err))
+}
