ae6d44d81c0488dd604277d10253592e64a7ddf4
Wessel Smit <wrsmit00@gmail.com>
2020-02-17 00:34:02 +0100

    rewrote cantinflas as a module

--- script.js
+++ script.js
@@ -26,120 +26,20 @@ routie({
 
 
 
+import * as template from './libraries/cantinflas.js';
 
-// * hieronder staat een compleet werkend voorbeeld, deze moet alleen nog omgeschreven worden naar een module
+const tmpl = `{{#name}}
+                Hello {{name}}!
+            {{/name}} 
 
-const template = `{
-    "name": {{ name }},
-    "hello": {{#obj}}{{name}}{{/obj}},
-    "version": {{version}},
-    {{^name}}
-    "description": {{description}},
-    {{/name}}
-    "keywordsmulti": [
-    {{#keywords}}
-        "{{name}}"
-    {{/keywords}}
-    ],
-    license: {{#license}}"MIT"{{/license}},
-    author: {{#func}}{{name}}{{/func}}
-}`
-
+            {{^name}} 
+                no name found 
+            {{/name}}`
 const data = {
-    name: 'Cantinflas',
-    version: '1.0.0',
-    description: 'Tiny mustache-like template engine',
-    obj: {
-        name: 'Hello'
-    },
-    keywords: [{
-        name: 'template'
-    }, {
-        name: 'mustache'
-    }, {
-        name: 'handlebars'
-    }],
-    license: true,
-    func: string => `Good Boy ${string}!`
+    name: 'Wessel'
 };
 
 
-
-const CHUNK = /[\t ]*({{([#^])+([\w\d.$@]+)}})[\s\S]*?({{\/\3}})[\t ]*\n?/g;
-const VARIABLE = /{{\s*(.+?)\s*}}/g
-
-const isObj = any => any === Object(any);
-const isArr = any => Array.isArray(any);
-const isFunc = any => typeof any === 'function';
-const isEmpObj = obj => Object.keys(obj).length === 0;
-const escRGX = str => str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-
-/**
- * Compile templates with Mustache-like syntax
- * @param {String} template Input string
- * @param {Object} data Data object
- * @returns {String} Compiled output string
- */
-function cantinflas(template, data) {
-    return render(template, data);
-
-    function render(fragment, context) {
-        return fragment.replace(CHUNK, (...[chunk, tagstart, meta, name, tagend]) => {
-                const INLINE = new RegExp(`^.*${escRGX(tagstart)}.*${escRGX(tagend)}.*$`, 'm');
-                const RGX = INLINE.test(chunk) ?
-                    new RegExp(`{{[#^]${escRGX(name)}}}([\\s\\S]*?)${escRGX(tagend)}`, 'g') :
-                    new RegExp(`\\s*{{[#^]${escRGX(name)}}}\\n?([\\s\\S]*?)[\\t ]*\\n?${escRGX(tagend)}\\s*`, 'g');
-
-                return chunk.replace(RGX, function (_, inner) {
-                    const val = value(name, context);
-                    if (meta === '#') {
-                        if (isArr(val)) {
-                            return val.reduce((str, crr, i) => {
-                                let ctx = {
-                                    ...context,
-                                    '.': crr,
-                                    '@index': i,
-                                    '@last': i === val.length - 1,
-                                    '@first': i === 0
-                                };
-                                if (isArr(crr) || isObj(crr)) ctx = {
-                                    ...ctx,
-                                    ...crr
-                                } // only spread objects and arrays - not strings
-                                return str += render(inner, ctx);
-                            }, '');
-                        }
-                        if (isFunc(val)) return render(val(inner, context), context);
-                        if (isObj(val)) return isEmpObj(val) ? '' : render(inner, {
-                            ...context,
-                            ...val
-                        });
-                        return !!val ? render(inner, context) : '';
-                    }
-                    if (meta === '^') return !val ? render(inner, context) : '';
-                })
-            })
-            .replace(VARIABLE, (_, key) => {
-                const val = value(key, context);
-                return val || val === 0 ? val : '';
-            });
-    }
-}
-
-/**
- * Extract values from objects using
- * strings with notation e.g `myobject.property`.
- * @param {String} key String to parse
- * @param {Object|Array} data Object to extract information from
- * @returns {Any} Value
- */
-function value(key, data) {
-    if (key === '.') return data['.'];
-    const parts = key.split('.');
-    while (parts.length) {
-        if (!(parts[0] in data)) return false;
-        data = data[parts.shift()];
-    }
-    return data;
-}
-document.querySelector('#overview div:last-of-type').innerHTML = cantinflas(template, data)
+document.querySelector('html').addEventListener('click', () => {
+    document.querySelector('#overview div:last-of-type').innerHTML = template.cantinflas(tmpl, data)
+})
